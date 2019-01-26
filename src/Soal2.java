6d645cab97789dbd7b08f2446fea742e0d83d0c7
Ahmad Khoirudin <bang.kho48@gmail.com>
2019-01-26 13:55:51 +0700

    Soal2 dari condition menjadi regex

--- src/Soal2.java
+++ src/Soal2.java
@@ -1,27 +1,23 @@
 import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Soal2 {
+    private static final String USERNAME = "^[a-z]{5}[_.]{1}[A-Z]{2}$";
+    private static Pattern pattern;
+    private static Matcher matcher;
+
     public static void main(String[] args) throws IOException {
         UsernameValidator("khoir.AK");
     }
 
-    private static void UsernameValidator(String in) {
-        if (in.length() == 8) {
-            if (in.substring(0, 5).equals(in.substring(0, 5).toLowerCase())) {
-                if (in.substring(5, 6).equals("_") || in.substring(5, 6).equals(".")) {
-                    if (in.substring(6, 8).equals(in.substring(6, 8).toUpperCase())) {
-                        System.out.println("Username Valid");
-                    } else {
-                        System.out.println("Username tidak valid. Username akhir salah");
-                    }
-                } else {
-                    System.out.println("Username tidak valid. Separator salah");
-                }
-            } else {
-                System.out.println("Username tidak valid. Username awal salah");
-            }
+    private static void UsernameValidator(String s) {
+        pattern = Pattern.compile(USERNAME);
+        matcher = pattern.matcher(s);
+        if (matcher.matches()) {
+            System.out.println("Username Valid");
         } else {
-            System.out.println("Username tidak valid. Lebih dari 8 karakter");
+            System.out.println("Username tidak valid.");
         }
     }
 }
